312d67ded767855070e827fc32726cb712b4b404
blackystardust <blackystardust@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2016-02-04 03:07:42 +0000

    Tagged VICE 2.4.25

---
+++ v2.4.25/vice/src/userport/userport.c
@@ -0,0 +1,530 @@
+/*
+ * userport.c - userport handling.
+ *
+ * Written by
+ *  Marco van den Heuvel <blackystardust68@yahoo.com>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "cmdline.h"
+#include "lib.h"
+#include "log.h"
+#include "machine.h"
+#include "resources.h"
+#include "translate.h"
+#include "uiapi.h"
+#include "userport.h"
+#include "util.h"
+
+static int userport_collision_handling = 0;
+static unsigned int order = 0;
+static userport_device_list_t userport_head = { NULL, NULL, NULL };
+static userport_port_props_t userport_props;
+
+/* ---------------------------------------------------------------------------------------------------------- */
+
+static int valid_device(userport_device_t *device)
+{
+    if ((device->read_pa2 || device->store_pa2) && !userport_props.has_pa2) {
+        return 0;
+    }
+
+    if ((device->read_pa3 || device->store_pa3) && !userport_props.has_pa3) {
+        return 0;
+    }
+
+    if ((device->read_flag || device->store_flag) && !userport_props.has_flag) {
+        return 0;
+    }
+
+    if (device->needs_pc && !userport_props.has_pc) {
+        return 0;
+    }
+
+    if ((device->store_sp1 || device->read_sp2) && !userport_props.has_sp12) {
+        return 0;
+    }
+
+    return 1;
+}
+
+/* ---------------------------------------------------------------------------------------------------------- */
+
+void userport_port_register(userport_port_props_t *props)
+{
+    userport_props.has_pa2 = props->has_pa2;
+    userport_props.has_pa3 = props->has_pa3;
+    userport_props.has_flag = props->has_flag;
+    userport_props.has_pc = props->has_pc;
+    userport_props.has_sp12 = props->has_sp12;
+}
+
+userport_device_list_t *userport_device_register(userport_device_t *device)
+{
+    userport_device_list_t *current = &userport_head;
+    userport_device_list_t *retval = NULL;
+
+    if (valid_device(device)) {
+        retval = lib_malloc(sizeof(userport_device_list_t));
+
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = retval;
+        retval->previous = current;
+        retval->device = device;
+        retval->next = NULL;
+        retval->device->order = order++;
+    }
+
+    return retval;
+}
+
+void userport_device_unregister(userport_device_list_t *device)
+{
+    userport_device_list_t *prev;
+
+    if (device) {
+        prev = device->previous;
+        prev->next = device->next;
+
+        if (device->next) {
+            device->next->previous = prev;
+        }
+
+        if (device->device->order == order - 1) {
+            if (order != 0) {
+                order--;
+            }
+        }
+
+        lib_free(device);
+    }
+}
+
+/* ---------------------------------------------------------------------------------------------------------- */
+
+static void userport_detach_devices(int collision, unsigned int highest_order)
+{
+    userport_device_list_t *current = userport_head.next;
+    char *tmp1 = lib_stralloc("Userport collision detected from ");
+    char *tmp2;
+    int col_found = 0;
+    char *last_device_resource = NULL;
+    char *last_device = NULL;
+    char **detach_resource_list = NULL;
+    int i;
+
+    if (userport_collision_handling == USERPORT_COLLISION_METHOD_DETACH_ALL) {
+        detach_resource_list = lib_malloc(sizeof(char *) * (collision + 1));
+        memset(detach_resource_list, 0, sizeof(char *) * (collision + 1));
+    }
+
+    while (current) {
+        if (current->device->collision) {
+            if (userport_collision_handling == USERPORT_COLLISION_METHOD_DETACH_ALL) {
+                detach_resource_list[col_found] = current->device->resource;
+            }
+            ++col_found;
+            if (current->device->order == highest_order) {
+                last_device_resource = current->device->resource;
+                last_device = current->device->name;
+            }
+            if (col_found == collision) {
+                tmp2 = util_concat(tmp1, "and ", current->device->name, NULL);
+            } else if (col_found == 1) {
+                tmp2 = util_concat(tmp1, current->device->name, NULL);
+            } else {
+                tmp2 = util_concat(tmp1, ", ", current->device->name, NULL);
+            }
+            lib_free(tmp1);
+            tmp1 = tmp2;
+        }
+        current = current->next;
+    }
+
+    if (userport_collision_handling == USERPORT_COLLISION_METHOD_DETACH_ALL) {
+        tmp2 = util_concat(tmp1, ". All involved devices will be detached.", NULL);
+        for (i = 0; detach_resource_list[i]; ++i) {
+            resources_set_int(detach_resource_list[i], 0);
+        }
+        lib_free(detach_resource_list);
+    } else {
+        tmp2 = util_concat(tmp1, ". Last device (", last_device, ") will be detached.", NULL);
+        resources_set_int(last_device_resource, 0);
+    }
+
+    lib_free(tmp1);
+    ui_error(tmp2);
+    lib_free(tmp2);
+}
+
+static BYTE userport_detect_collision(BYTE retval_orig, BYTE mask)
+{
+    BYTE retval = retval_orig;
+    BYTE rm;
+    BYTE rv;
+    int collision = 0;
+    int first_found = 0;
+    userport_device_list_t *current = userport_head.next;
+    unsigned int highest_order = 0;
+
+    /* collision detection */
+    current = userport_head.next;
+
+    while (current) {
+        if (current->device->read_pbx != NULL) {
+            rm = current->device->mask;
+            rm &= mask;
+            if (rm) {
+                rv = current->device->retval;
+                rv |= ~rm;
+                rv = 0xff & rv;
+                if (!first_found) {
+                    retval = rv;
+                    first_found = 1;
+                    current->device->collision = 1;
+                    if (highest_order < current->device->order) {
+                        highest_order = current->device->order;
+                    }
+                } else {
+                    if (rv != retval) {
+                        ++collision;
+                        current->device->collision = 1;
+                        if (highest_order < current->device->order) {
+                            highest_order = current->device->order;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (collision) {
+        userport_detach_devices(collision + 1, highest_order);
+        if (userport_collision_handling == USERPORT_COLLISION_METHOD_DETACH_ALL) {
+            retval = 0xff;
+        }
+    }
+
+    return retval;
+}
+
+/* ---------------------------------------------------------------------------------------------------------- */
+
+BYTE read_userport_pbx(BYTE mask)
+{
+    BYTE retval = 0xff;
+    BYTE rm;
+    BYTE rv;
+    int valid = 0;
+    userport_device_list_t *current = userport_head.next;
+
+    if (!mask) {
+        return 0xff;
+    }
+
+    /* set retval */
+    while (current) {
+        current->device->collision = 0;
+        if (current->device->read_pbx != NULL) {
+            current->device->read_pbx();
+            rm = current->device->mask;
+            rm &= mask;
+            if (rm) {
+                rv = current->device->retval;
+                rv |= ~rm;
+                retval &= rv;
+                ++valid;
+            }
+        }
+        current = current->next;
+    }
+
+    if (valid > 1 && userport_collision_handling != USERPORT_COLLISION_METHOD_AND_WIRES) {
+        return userport_detect_collision(retval, mask);
+    }
+
+    return retval;
+}
+
+void store_userport_pbx(BYTE val)
+{
+    userport_device_list_t *current = userport_head.next;
+
+    while (current) {
+        if (current->device->store_pbx != NULL) {
+            current->device->store_pbx(val);
+        }
+        current = current->next;
+    }
+}
+
+BYTE read_userport_pa2(BYTE orig)
+{
+    BYTE mask = 1;
+    BYTE rm;
+    BYTE rv;
+    BYTE retval = 0xff;
+    int valid = 0;
+    userport_device_list_t *current = userport_head.next;
+
+    /* set retval */
+    while (current) {
+        current->device->collision = 0;
+        if (current->device->read_pa2 != NULL) {
+            current->device->read_pa2();
+            rm = current->device->mask;
+            rm &= mask;
+            if (rm) {
+                rv = current->device->retval;
+                rv |= ~rm;
+                retval &= rv;
+                ++valid;
+            }
+        }
+        current = current->next;
+    }
+
+    if (valid > 1 && userport_collision_handling != USERPORT_COLLISION_METHOD_AND_WIRES) {
+        return userport_detect_collision(retval, mask);
+    }
+    if (valid == 0) {
+        return orig;
+    }
+
+    return retval;
+}
+
+void store_userport_pa2(BYTE val)
+{
+    userport_device_list_t *current = userport_head.next;
+
+    while (current) {
+        if (current->device->store_pa2 != NULL) {
+            current->device->store_pa2(val);
+        }
+        current = current->next;
+    }
+}
+
+BYTE read_userport_pa3(BYTE orig)
+{
+    BYTE mask = 1;
+    BYTE rm;
+    BYTE rv;
+    BYTE retval = 0xff;
+    int valid = 0;
+    userport_device_list_t *current = userport_head.next;
+
+    /* set retval */
+    while (current) {
+        current->device->collision = 0;
+        if (current->device->read_pa3 != NULL) {
+            current->device->read_pa3();
+            rm = current->device->mask;
+            rm &= mask;
+            if (rm) {
+                rv = current->device->retval;
+                rv |= ~rm;
+                retval &= rv;
+                ++valid;
+            }
+        }
+        current = current->next;
+    }
+
+    if (valid > 1 && userport_collision_handling != USERPORT_COLLISION_METHOD_AND_WIRES) {
+        return userport_detect_collision(retval, mask);
+    }
+    if (valid == 0) {
+        return orig;
+    }
+
+    return retval;
+}
+
+void store_userport_pa3(BYTE val)
+{
+    userport_device_list_t *current = userport_head.next;
+
+    while (current) {
+        if (current->device->store_pa3 != NULL) {
+            current->device->store_pa3(val);
+        }
+        current = current->next;
+    }
+}
+
+BYTE read_userport_flag(void)
+{
+    BYTE mask = 1;
+    BYTE rm;
+    BYTE rv;
+    BYTE retval = 0xff;
+    int valid = 0;
+    userport_device_list_t *current = userport_head.next;
+
+    /* set retval */
+    while (current) {
+        current->device->collision = 0;
+        if (current->device->read_flag != NULL) {
+            current->device->read_flag();
+            rm = current->device->mask;
+            rm &= mask;
+            if (rm) {
+                rv = current->device->retval;
+                rv |= ~rm;
+                retval &= rv;
+                ++valid;
+            }
+        }
+        current = current->next;
+    }
+
+    if (valid > 1 && userport_collision_handling != USERPORT_COLLISION_METHOD_AND_WIRES) {
+        return userport_detect_collision(retval, mask);
+    }
+
+    return retval;
+}
+
+void store_userport_flag(BYTE val)
+{
+    userport_device_list_t *current = userport_head.next;
+
+    while (current) {
+        if (current->device->store_flag != NULL) {
+            current->device->store_flag(val);
+        }
+        current = current->next;
+    }
+}
+
+void store_userport_sp1(void)
+{
+    userport_device_list_t *current = userport_head.next;
+
+    while (current) {
+        if (current->device->store_sp1 != NULL) {
+            current->device->store_sp1();
+        }
+        current = current->next;
+    }
+}
+
+BYTE read_userport_sp2(BYTE orig)
+{
+    BYTE mask = 0xff;
+    BYTE rm;
+    BYTE rv;
+    BYTE retval = 0xff;
+    int valid = 0;
+    userport_device_list_t *current = userport_head.next;
+
+    /* set retval */
+    while (current) {
+        current->device->collision = 0;
+        if (current->device->read_sp2 != NULL) {
+            current->device->read_sp2();
+            rm = current->device->mask;
+            rm &= mask;
+            if (rm) {
+                rv = current->device->retval;
+                rv |= ~rm;
+                retval &= rv;
+                ++valid;
+            }
+        }
+        current = current->next;
+    }
+
+    if (valid > 1 && userport_collision_handling != USERPORT_COLLISION_METHOD_AND_WIRES) {
+        return userport_detect_collision(retval, mask);
+    }
+
+    if (!valid) {
+        return orig;
+    }
+
+    return retval;
+}
+
+/* ---------------------------------------------------------------------------------------------------------- */
+
+static int set_userport_collision_handling(int val, void *param)
+{
+    switch (val) {
+        case USERPORT_COLLISION_METHOD_DETACH_ALL:
+        case USERPORT_COLLISION_METHOD_DETACH_LAST:
+        case USERPORT_COLLISION_METHOD_AND_WIRES:
+            break;
+        default:
+            return -1;
+    }
+    userport_collision_handling = val;
+
+    return 0;
+}
+
+static const resource_int_t resources_int[] = {
+    { "UserportCollisionHandling", USERPORT_COLLISION_METHOD_DETACH_ALL, RES_EVENT_STRICT, (resource_value_t)0,
+      &userport_collision_handling, set_userport_collision_handling, NULL },
+    { NULL }
+};
+
+int userport_resources_init(void)
+{
+    if (resources_register_int(resources_int) < 0) {
+        return -1;
+    }
+
+    return machine_register_userport();
+}
+
+void userport_resources_shutdown(void)
+{
+    userport_device_list_t *current = userport_head.next;
+
+    while (current) {
+        userport_device_unregister(current);
+        current = userport_head.next;
+    }
+}
+
+static const cmdline_option_t cmdline_options[] = {
+    { "-userportcollision", SET_RESOURCE, 1,
+      NULL, NULL, "UserportCollisionHandling", NULL,
+      USE_PARAM_ID, USE_DESCRIPTION_ID,
+      IDCLS_P_METHOD, IDCLS_SELECT_USERPORT_CONFLICT_HANDLING,
+      NULL, NULL },
+    { NULL }
+};
+
+int userport_cmdline_options_init(void)
+{
+    return cmdline_register_options(cmdline_options);
+}
---
+++ v2.4.25/vice/src/userport/userport_digimax.c
@@ -0,0 +1,192 @@
+/*
+ * userport_digimax.c - Digimax DAC userport device emulation.
+ *
+ * Written by
+ *  Marco van den Heuvel <blackystardust68@yahoo.com>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cmdline.h"
+#include "lib.h"
+#include "machine.h"
+#include "maincpu.h"
+#include "resources.h"
+#include "sound.h"
+#include "uiapi.h"
+#include "userport.h"
+#include "userport_digimax.h"
+#include "util.h"
+#include "translate.h"
+
+#include "digimaxcore.c"
+
+/*
+    Digimax Cartridge
+
+    This cartridge is an 8bit 4-channel digital sound output
+    interface.
+
+C64/C128 | CBM2 | TLC7226 DAC | NOTES
+-------------------------------------
+    8    |  5   |      15     | /PC2 -> /Write
+    9    |  4   |      16     | /PA3 -> A1
+    C    | 14   |      14     | PB0 -> DB0
+    D    | 13   |      13     | PB1 -> DB1
+    E    | 12   |      12     | PB2 -> DB2
+    F    | 11   |      11     | PB3 -> DB3
+    H    | 10   |      10     | PB4 -> DB4
+    J    |  9   |       9     | PB5 -> DB5
+    K    |  8   |       8     | PB6 -> DB6
+    L    |  7   |       7     | PB7 -> DB7
+    M    |  2   |      17     | PA2 -> A0
+*/
+
+/* Some prototypes are needed */
+static void userport_digimax_store_pbx(BYTE value);
+static void userport_digimax_store_pa2(BYTE value);
+static void userport_digimax_store_pa3(BYTE value);
+
+static userport_device_t digimax_device = {
+    "Userport DigiMAX",
+    IDGS_USERPORT_DIGIMAX,
+    NULL, /* NO pbx read */
+    userport_digimax_store_pbx,
+    NULL, /* NO pa2 read */
+    userport_digimax_store_pa2,
+    NULL, /* NO pa3 read */
+    userport_digimax_store_pa3,
+    NULL, /* NO flag read */
+    NULL, /* NO flag write */
+    0, /* NO pc pin needed */
+    NULL, /* NO sp1 write */
+    NULL, /* NO sp2 read */
+    "UserportDigiMax",
+    0xff,
+    0xf, /* validity mask doesn't change */
+    0,
+    0
+};
+
+static userport_device_list_t *userport_digimax_list_item = NULL;
+
+/* ------------------------------------------------------------------------- */
+
+static BYTE userport_digimax_address = 3;
+
+void userport_digimax_sound_chip_init(void)
+{
+    digimax_sound_chip_offset = sound_chip_register(&digimax_sound_chip);
+}
+
+static void userport_digimax_store_pa2(BYTE value)
+{
+    userport_digimax_address &= 2;
+    userport_digimax_address |= (value & 1);
+}
+
+static void userport_digimax_store_pa3(BYTE value)
+{
+    userport_digimax_address &= 1;
+    userport_digimax_address |= ((value & 1) << 1);
+}
+
+static void userport_digimax_store_pbx(BYTE value)
+{
+    BYTE addr = 0;
+
+    switch (userport_digimax_address) {
+        case 0x0:
+            addr = 2;
+            break;
+        case 0x4:
+            addr = 3;
+            break;
+        case 0x8:
+            addr = 0;
+            break;
+        case 0xc:
+            addr = 1;
+            break;
+    }
+
+    digimax_sound_data[addr] = value;
+    sound_store((WORD)(digimax_sound_chip_offset | addr), value, 0);
+}
+
+/* ---------------------------------------------------------------------*/
+
+static int set_digimax_enabled(int value, void *param)
+{
+    int val = value ? 1 : 0;
+
+    if (!digimax_sound_chip.chip_enabled && val) {
+        userport_digimax_list_item = userport_device_register(&digimax_device);
+        digimax_sound_chip.chip_enabled = 1;
+    } else if (digimax_sound_chip.chip_enabled && !val) {
+        if (userport_digimax_list_item != NULL) {
+            userport_device_unregister(userport_digimax_list_item);
+            userport_digimax_list_item = NULL;
+        }
+        digimax_sound_chip.chip_enabled = 0;
+    }
+    return 0;
+}
+
+/* ---------------------------------------------------------------------*/
+
+static const resource_int_t resources_int[] = {
+    { "UserportDIGIMAX", 0, RES_EVENT_STRICT, (resource_value_t)0,
+      &digimax_sound_chip.chip_enabled, set_digimax_enabled, NULL },
+    { NULL }
+};
+
+int userport_digimax_resources_init(void)
+{
+    return resources_register_int(resources_int);
+}
+
+/* ---------------------------------------------------------------------*/
+
+static const cmdline_option_t cmdline_options[] =
+{
+    { "-userportdigimax", SET_RESOURCE, 0,
+      NULL, NULL, "UserportDIGIMAX", (resource_value_t)1,
+      USE_PARAM_STRING, USE_DESCRIPTION_ID,
+      IDCLS_UNUSED, IDCLS_ENABLE_USERPORT_DIGIMAX,
+      NULL, NULL },
+    { "+userportdigimax", SET_RESOURCE, 0,
+      NULL, NULL, "UserportDIGIMAX", (resource_value_t)0,
+      USE_PARAM_STRING, USE_DESCRIPTION_ID,
+      IDCLS_UNUSED, IDCLS_DISABLE_USERPORT_DIGIMAX,
+      NULL, NULL },
+    { NULL }
+};
+
+int userport_digimax_cmdline_options_init(void)
+{
+    return cmdline_register_options(cmdline_options);
+}
