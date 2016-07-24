ac2997f1b75ec14363be4b0e53a020ca2f7b0483
blackystardust <blackystardust@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2016-07-24 23:27:04 +0000

    Tagged VICE 2.4.29

---
+++ v2.4.29/vice/src/arch/os2/watcom/genwt.bat
@@ -0,0 +1,33 @@
+@echo off
+echo "Generating files for windows"
+copy ..\..\..\translate.txt trans.txt
+copy ..\..\..\..\doc\vice.texi vice.tex
+copy ..\..\..\infocontrib.sed ic.sed
+copy ..\..\..\..\AUTHORS authors.txt
+copy ..\..\unix\macosx\Resources\Credits.html credits.htm
+copy ..\..\..\..\README readme.tx1
+copy ..\..\..\..\doc\html\index.html index.htm
+copy ..\..\os2\dialogs\dialogs.rc dlgs.rc
+copy ..\..\..\..\man\vice.1 vice1.txt
+if Windows_NT == %OS% goto winnt
+goto use16
+:winnt
+if defined ProgramFiles(x86) GOTO use32
+:use16
+genic ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.tx1 watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
+gentr trans.txt trans.h transtab.h
+goto after
+:use32
+geninfocontrib32 ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.tx1 watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
+gentranslate32 trans.txt trans.h transtab.h
+:after
+copy ic.h ..\..\..\infocontrib.h 
+copy trans.h ..\..\..\translate.h
+copy transtab.h ..\..\..\translate_table.h
+copy authors.txt ..\..\..\..\AUTHORS
+copy credits.htm ..\..\unix\macosx\Resources\Credits.html
+copy readme.tx1 ..\..\..\..\README
+copy index.htm ..\..\..\..\doc\html\index.html
+copy dlgs.rc ..\..\os2\dialogs\dialogs.rc
+copy vice1.txt ..\..\..\..\man\vice.1
+del ic.h vice.tex ic.sed trans.txt trans.h transtab.h authors.txt credits.htm readme.tx1 index.htm dlgs.rc vice1.txt
---
+++ v2.4.29/vice/src/arch/sdl/generate_msvc14_sdl2x.bat
@@ -0,0 +1,36 @@
+@echo off
+if not exist ..\win32\vs_tmpl\mkmsvc.exe goto missingmkmsvc
+echo generating SDL2x MSVC14 project as win32-sdl2x-msvc14
+if exist win32-sdl2x-msvc14 goto removedir
+:makemsvcdir
+mkdir win32-sdl2x-msvc14
+cd ..\win32\vs_tmpl
+mkmsvc.exe -sdl2 -14 vice
+cd ..\..\sdl
+copy msvc-files\make-bindist-sdl2x.bat.proto win32-sdl2x-msvc14\make-msvc-bindist.bat
+copy msvc-files\debug.h win32-sdl2x-msvc14
+copy msvc-files\dirent.h win32-sdl2x-msvc14
+copy msvc-files\ide-config-sdl2.h win32-sdl2x-msvc14\ide-config.h
+copy msvc-files\ide-siddefs.h win32-sdl2x-msvc14
+copy msvc-files\ide-siddtvdefs.h win32-sdl2x-msvc14
+copy msvc-files\inttypes.h win32-sdl2x-msvc14
+copy msvc-files\stdint.h win32-sdl2x-msvc14
+copy msvc-files\ffmpeg-config.h win32-sdl2x-msvc14
+copy msvc-files\vice.manifest win32-sdl2x-msvc14
+copy msvc-files\winid_arm.bat win32-sdl2x-msvc14
+copy msvc-files\winid_x64.bat win32-sdl2x-msvc14
+copy msvc-files\winid_x86.bat win32-sdl2x-msvc14
+echo SDL2x MSVC14 project files generated in win32-sdl2x-msvc14
+goto end
+:missingmkmsvc
+echo ..\win32\vs_tmpl\mkmsvc.exe is missing
+goto end
+:removedir
+echo y | del win32-sdl2x-msvc14\*.* /S
+rd win32-sdl2x-msvc14 /s /q
+if exist win32-sdl2x-msvc14 goto cannotremovedir
+goto makemsvcdir
+:cannotremovedir
+echo can't delete the win32-sdl2x-msvc14 directory, please remove win32-sdl2x-msvc14 manually and run this batch file again.
+:end
+pause
